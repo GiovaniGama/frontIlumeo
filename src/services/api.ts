b96f328426b48fbfd521cccd25f6cd96a0de343f
Giovani Gama <giovanigama.08@hotmail.com>
2023-03-23 12:25:23 -0300

    header auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,17 +5,29 @@ export const LoginUser = async (user_name: string, password: string) => {
 }
 
 export const createWorkSchedule = async (date_check_in: Date, date_check_out: Date) => {
-    return http.post('/create-schedule', {  date_check_in, date_check_out })
+    return http.post('/create-schedule', {  date_check_in, date_check_out },
+    {
+        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
 }
 
 export const getWorkSchedule = async() => {
-    return http.get('/schedule')
+    return http.get('/schedule', 
+    {
+        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
 }
 
 export const getUser = async() => {
-    return http.get('/profile')
+    return http.get('/profile',
+    {
+        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
 }
 
 export const createUser = async(user_name: string, password: string) => {
-    return http.post('/user', { user_name, password })
+    return http.post('/user', { user_name, password },
+    {
+        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
 }
