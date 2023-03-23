cbfc7e8b65e4185ffbca883f4de5189e84e2a2f6
Giovani Gama <giovanigama.08@hotmail.com>
2023-03-22 23:03:09 -0300

    url prod

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { environment } from "../environments/environments";
+import { environment } from "../environments/environments.prod";
 
 const url_api = environment.apiURL
 
